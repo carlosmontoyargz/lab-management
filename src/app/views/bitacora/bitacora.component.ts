a05d336e6bb2afaea394a5207b4dc75db4137b4d
Carlos Montoya <carlos.montoya.rdgz@gmai.com>
2022-02-13 04:07:15 -0600

    modulo para bitacora

---
+++ src/app/views/bitacora/bitacora.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './bitacora.component.html',
+  styleUrls: ['./bitacora.component.scss']
+})
+export class BitacoraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
